72f37e18ae77ce2a1b3c935c0d3d0cc81bac764a
Zihao Yu <yuzihao@ict.ac.cn>
2019-12-10 20:38:39 +0800

    add projectn logo

--- src/main.c
+++ src/main.c
@@ -7,13 +7,16 @@ void init_irq(void);
 void init_fs(void);
 void init_proc(void);
 
+extern const char logo[];
+
 int main() {
+  printf("%s", logo);
+  Log("'Hello World!' from Nanos-lite");
+  Log("Build time: %s, %s", __TIME__, __DATE__);
+
 #ifdef HAS_VME
   init_mm();
 #endif
-
-  Log("'Hello World!' from Nanos-lite");
-  Log("Build time: %s, %s", __TIME__, __DATE__);
 
   init_ramdisk();
 
